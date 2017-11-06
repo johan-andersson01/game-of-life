024b7f084bf4ebb0ac75475175aa6678bc8a2687
Johan <johan.andersson95@gmail.com>
2017-11-06 19:14:13 +0100

    optimize 1.0 complete

--- game.js
+++ game.js
@@ -1,130 +1,138 @@
-/*jslint browser:true */
-/*jslint plusplus: true */
-var canvas, cc;
-var rps;
-var cellSize;
-var prevRound, thisRound, rows, cols, scarcity;
+"use strict";
+let canvas, cc;
+let cellSize;
+let aliveCells, currentState, nextState, rows, cols;
+let pad = 2; // padding
 
-function initCanvas() {
-    "use strict";
+const initCanvas = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     cols = Math.round(window.innerWidth / cellSize);
     rows = Math.round(window.innerHeight / cellSize);
-    prevRound = [];
-    prevRound.length = cols;
-    var x, y, setAlive = 0;
+    currentState = [];
+    currentState.length = cols;
+    aliveCells = [];
+    let x, y;
     for (x = 0; x < cols; x++) {
-        prevRound[x] = [];
-        prevRound[x].length = rows;
+        currentState[x] = [];
+        currentState[x].length = rows;
         for (y = 0; y < rows; y++) {
-            if (x === 0 || y === 0 || x === cols - 1 || y === rows - 1 ) {
-                prevRound[x][y] = 0;
+            if (x <= pad || y <= pad || x >= cols - pad || y >= rows - pad ) {
+                currentState[x][y] = 0;
 
             } else {
-                prevRound[x][y] = Math.round(Math.random() - scarcity);
+                currentState[x][y] = Math.round(Math.random() - 0.3);
+                if (currentState[x][y] === 1) {
+                    aliveCells.push([x, y]);
+                }
             }
         }
     }
+    console.log(aliveCells.length);
+    console.log(cols*rows);
 
-
-    thisRound = [];
-    thisRound.length = rows;
+    nextState = [];
+    nextState.length = rows;
     for (x = 0; x < cols; x++) {
-        thisRound[x] = [];
-        thisRound[x].length = rows;
+        nextState[x] = [];
+        nextState[x].length = rows;
         for (y = 0; y < rows; y++) {
-            thisRound[x][y] = 0;
+            nextState[x][y] = currentState[x][y];
         }
     }
 }
 
-
-function getNeighbours(x, y) {
-    "use strict";
-    var neighbours = [],
-        i, k;
-    neighbours.length = 8;
-    for (i = -1; i < 2; i++) {
-        for (k = -1; k < 2; k++) {
-            if (!(i === 0 && k === 0)) {
-                neighbours.push(prevRound[x + i][y + k]);
+const getNeighbours = (x, y) => {
+    let neighbours = [];
+    for (let i = -1; i < 2; i++) {
+        for (let k = -1; k < 2; k++) {
+            if (i != 0 || k != 0) {
+                neighbours.push([x + i, y + k]);
             }
         }
     }
     return neighbours;
 }
 
-function survival(x, y) {
-    "use strict";
-    var neighbours = getNeighbours(x, y),
-        aliveNeighbours = 0,
-        wasIalive = prevRound[x][y],
-        survive = 0,
-        j;
-    for (j = 0; j < neighbours.length; j++) {
-        if (neighbours[j] === 1) {
-            aliveNeighbours++;
-        }
-    }
-
-    if (wasIalive && (aliveNeighbours < 2 || aliveNeighbours > 3)) {
-        survive = 0;
-    } else if (wasIalive && (aliveNeighbours === 2 || aliveNeighbours === 3)) {
-        survive = 1;
-    } else if (!wasIalive && aliveNeighbours === 3) {
-        survive = 1;
-    }
-    return survive;
-}
-
-function applyRules() {
-    "use strict";
-    var terminate = true,
-        x, y;
-    for (x = 0; x < cols; x++) {
-        for (y = 0; y < rows; y++) {
-            if (x < 2 || y < 2 || x > cols - 2 || y > rows - 2) {
-                thisRound[x][y] = 0;
-            } else {
-                thisRound[x][y] = survival(x, y);
-                if (prevRound[x][y] !== thisRound[x][y]) {
-                    terminate = false;
+const nbrOfAliveNeighbours = (x, y) => {
+    let alive = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let k = -1; k < 2; k++) {
+            if (i != 0 || k != 0) {
+                if (currentState[x + i][y + k] === 1) {
+                    alive += 1;
                 }
             }
-            prevRound[x][y] = thisRound[x][y];
-            if (thisRound[x][y] === 1) {
-                cc.fillStyle = '#E84C0C';
-            } else {
-                cc.fillStyle = 'white';
-            }
-            cc.fillRect(x * cellSize, y * cellSize, (x + 1) * cellSize,
-                (y + 1) * cellSize);
         }
-    }
-    return terminate;
+    }   
+    return alive;
 }
 
-function drawCanvas() {
-    "use strict";
-    cc.fillStyle = 'white';
-    cc.fillRect(0, 0, canvas.width, canvas.height);
-    return applyRules();
+const calcNextState = (x, y) => {
+    var aliveNeighbours = nbrOfAliveNeighbours(x, y);
+    if (currentState[x][y] === 1 && (aliveNeighbours < 2 || aliveNeighbours > 3)) {
+        nextState[x][y] = 0;
+    } else if (currentState[x][y] === 1 && (aliveNeighbours === 2 || aliveNeighbours === 3)) {
+        nextState[x][y] = 1;
+    } else if (currentState[x][y] === 0 && aliveNeighbours === 3) {
+        nextState[x][y] = 1;
+    }
+}
+
+const survival = (x, y) => {
+    calcNextState(x, y);
+    const neighbours = getNeighbours(x, y);
+    for (const position of neighbours) {
+        const x1 = position[0];
+        const y1 = position[1];
+        if (x1 > pad && x1 < cols - pad && y1 > pad && y1 < rows - pad) {
+            if (currentState[x1][y1] === 0) {
+                calcNextState(x1, y1);
+            }
+        }
+    }
+}
+
+const nextRound = () => {
+    // console.log(aliveCells.length);
+    for (const position of aliveCells) {
+        survival(position[0], position[1]);
+    }
+    aliveCells = [];
+    let noProgress = true;
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
+            if (currentState[x][y] != nextState[x][y]) {
+                noProgress = false;
+                currentState[x][y] = nextState[x][y];
+            }
+            if (currentState[x][y] === 1) {
+                cc.fillStyle = 'green';
+                aliveCells.push([x,y]);
+            }
+            else {
+                cc.fillStyle = 'black';
+            }
+            cc.fillRect(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+        }
+    }
+
+    if (noProgress) {
+        console.log("terminate");
+        return false;
+    }
+    return true;
 }
 
 window.onload = function() {
-    "use strict";
     canvas = document.getElementById('canvas');
     cc = canvas.getContext('2d');
-    rps = window.prompt("Set rounds per second", "1");
-    cellSize = window.prompt("Set cell size (px)", "10");
-    scarcity = parseFloat(window.prompt("Set cell scarcity (min: 0, max: 5)", "3"))/10;
+    cellSize = 15;
     initCanvas();
     var refresherID = setInterval(function() {
-        if (drawCanvas()) {
+        if (!nextRound()) {
             clearInterval(refresherID);
         }
 
-    }, 1000 / rps);
-
+    }, 500);
 };
